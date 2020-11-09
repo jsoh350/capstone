716462763da516dd83cb2bba755735872c1cd5d2
jsoh350 <62550633+jsoh350@users.noreply.github.com>
2020-11-09 11:16:37 +0900

    Update views.py

--- capstone/views.py
+++ capstone/views.py
@@ -1,6 +1,12 @@
-from django.http import HttpResponse
+import urllib
+
+from django.contrib.postgres import serializers
+from django.http import HttpResponse, Http404
+from django.views.decorators.csrf import csrf_exempt
+
 from .models import Sentence, Record
 
+@csrf_exempt
 def results(request, sentence):
     if request.method == 'POST':
             sentence = Sentence()
@@ -15,6 +21,7 @@ def results(request, sentence):
             sentence.save()
             return HttpResponse()
 
+@csrf_exempt
 def complete(request, sentence):
     if request.method == 'POST':
         record = Record.objects.all()
@@ -25,6 +32,7 @@ def complete(request, sentence):
         record.total_score = request.POST['total_score']
     return HttpResponse()
 
+@csrf_exempt
 def leaderboard(request, input_type):
     if request.method == 'GET':
         lb = Record.objects.all()
@@ -33,6 +41,9 @@ def leaderboard(request, input_type):
         lb1 = lb1.order_by('total_score')
         lb1 = lb1[:5]
         for model_instance in lb1:
-            print(lb1.user_email[:3])
-            print(lb1.total_score)
-            
+         str1 = lb1[0].user_email[:3] + lb1[0].avg_speed + lb1[0].avg_accuracy + lb1[0].input_type + lb1[0].total_score
+         str2 = lb1[1].user_email[:3] + lb1[1].avg_speed + lb1[1].avg_accuracy + lb1[1].input_type + lb1[1].total_score
+         str3 = lb1[2].user_email[:3] + lb1[2].avg_speed + lb1[2].avg_accuracy + lb1[2].input_type + lb1[2].total_score
+         str4 = lb1[3].user_email[:3] + lb1[3].avg_speed + lb1[3].avg_accuracy + lb1[3].input_type + lb1[3].total_score
+         str5 = lb1[4].user_email[:3] + lb1[4].avg_speed + lb1[4].avg_accuracy + lb1[4].input_type + lb1[4].total_score
+        return HttpResponse(str(str1), str(str2), str(str3), str(str4), str(str5))
