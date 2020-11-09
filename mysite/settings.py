aa5ecd037fc6a3eceece647c660c624dfcc18f65
jsoh350 <62550633+jsoh350@users.noreply.github.com>
2020-11-09 11:27:23 +0900

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['127.0.0.1','.pythonanywhere.com']
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'capstone.apps.CapstoneConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'mysite.urls'
